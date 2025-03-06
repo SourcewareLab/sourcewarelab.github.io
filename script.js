20c745b8a140ea80a41e338ba8c06ebaa78e06d7
Aman14459hrc <mr.aman2021@gmail.com>
2025-03-06 18:19:01 +0530

    02

--- script.js
+++ script.js
@@ -29,3 +29,33 @@ document.querySelectorAll(".nav-links a").forEach(link => {
         toggleMenu()
     })
 })
+
+const paragraphs = document.querySelectorAll('.qa-card > p');
+paragraphs.forEach(paragraph => {
+    paragraph.style.display = 'flex';
+    paragraph.style.justifySelf = 'center';
+    paragraph.style.maxWidth = '70%';
+    
+});
+
+// Select all the paragraph elements within sections and hide them
+const sectionParagraphs = document.querySelectorAll('.qa-card p');
+sectionParagraphs.forEach(para => {
+    para.style.display = "none";
+});
+
+const head = document.querySelectorAll(".question")
+head.forEach(header =>{
+    header.addEventListener("click",()=>{
+                
+        sectionParagraphs.forEach(para =>{
+            if (para.style.display ==="none") {
+                
+                para.style.display = "block";
+            }else{
+                
+                para.style.display = "none";
+            }
+        })
+    })
+})
